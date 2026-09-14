29be1b501b205bd4097e9913e5a2dc3eba4c446e
Ashvinda Bandara <ashvindabandara75@gmail.com>
2026-09-14 21:15:55 +0530

    Add files via upload

---
+++ Supplier API 1/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Supplier_API_1.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        public string Status { get; set; } = "Pending";
+
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ Supplier API 1/Models/Quote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Supplier_API_1.Models
+{
+    public class Quote
+    {
+        [Key]
+        public int QuoteId { get; set; }
+
+        public DateTime QuoteDate { get; set; } = DateTime.Now;
+
+        public string Status { get; set; } = "Pending";
+
+        public List<QuoteItem> QuoteItems { get; set; } = new List<QuoteItem>();
+    }
+}
