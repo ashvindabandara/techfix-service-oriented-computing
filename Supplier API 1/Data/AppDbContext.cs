483b90a2adad2e1c90e2565e4054ea321c928cd9
Ashvinda Bandara <ashvindabandara75@gmail.com>
2026-09-14 21:13:29 +0530

    Add files via upload

---
+++ Supplier API 1/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Supplier_API_1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Supplier_API_1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<Quote> Quotes { get; set; }
+
+        public DbSet<QuoteItem> QuoteItems { get; set; }
+    }
+}
