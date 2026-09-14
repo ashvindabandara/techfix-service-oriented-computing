7ceeb9f0dfd1dc32e4f0f64bdde015cc7d091473
Ashvinda Bandara <ashvindabandara75@gmail.com>
2026-09-14 21:14:50 +0530

    Add files via upload

---
+++ Supplier API 1/Migrations/20240914183208_ChangeProductModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Supplier_API_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeProductModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductCode",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductCode",
+                table: "Products");
+        }
+    }
+}
---
+++ Supplier API 1/Migrations/20240915162033_UpdateModelsOrderFourth.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Supplier_API_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelsOrderFourth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
