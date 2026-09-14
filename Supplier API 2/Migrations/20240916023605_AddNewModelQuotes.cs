84cb441e7d37add94e1819f470cff561188c2cf4
Ashvinda Bandara <ashvindabandara75@gmail.com>
2026-09-14 21:22:14 +0530

    Add files via upload

---
+++ Supplier API 2/Migrations/20240916023605_AddNewModelQuotes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Supplier_API_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewModelQuotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    QuoteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuoteDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.QuoteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuoteItems",
+                columns: table => new
+                {
+                    QuoteItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    QuoteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuoteItems", x => x.QuoteItemId);
+                    table.ForeignKey(
+                        name: "FK_QuoteItems_Quotes_QuoteId",
+                        column: x => x.QuoteId,
+                        principalTable: "Quotes",
+                        principalColumn: "QuoteId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuoteItems_QuoteId",
+                table: "QuoteItems",
+                column: "QuoteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuoteItems");
+
+            migrationBuilder.DropTable(
+                name: "Quotes");
+        }
+    }
+}
